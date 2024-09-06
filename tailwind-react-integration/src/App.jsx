1f1021f2271c2e3a8e50a0e75222fb66cf9f99c0
Yaseen Alam El-Deen <161075877+yaseenalmeldin@users.noreply.github.com>
2024-09-06 16:25:13 +0300

    Add files via upload

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import UserProfile from "./components/UserProfile";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <UserProfile />
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,20 @@
+function UserProfile() {
+  return (
+    <div className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+      />
+      <h1 className="md:text-xl sm:text-lg text-blue-800 hover:text-blue-500 my-4">
+        John Doe
+      </h1>
+      <p className="text-gray-600 md:text-base sm:text-sm">
+        Developer at Example Co. Loves to write code and explore new
+        technologies.
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
